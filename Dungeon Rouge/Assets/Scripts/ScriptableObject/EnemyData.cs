bdb748399d83dcdb6443856382dba0bf5e4028d4
CHICHI <smartb12@naver.com>
2024-06-19 15:00:41 +0900

    DataManager, ScriptableObject

---
+++ Dungeon Rouge/Assets/Scripts/ScriptableObject/EnemyData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+[CreateAssetMenu(fileName = "EnemyData", menuName = "SriptableObjects/EnemyData", order = 2)]
+public class EnemyData : ScriptableObject
+{
+    [Header("EnemyInfo")]
+    public int enemyInfo;
+    public float enemyAtk;
+    public float enemyHealth;
+}
+
---
+++ Dungeon Rouge/Assets/Scripts/ScriptableObject/PlayerData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PlayerData", menuName = "ScriptableObjects/PlayerData", order = 1)]
+public class PlayerData : ScriptableObject
+{
+    [Header("PlayerInfo")]
+    public int playerLv;
+    public float playerAtk;
+    public float playerMaxHealth;
+}
