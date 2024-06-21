5ef17436193b96fd468c37eccd70e4652f287ac8
J-Kaun <kangeun9727@gmail.com>
2024-06-21 13:07:31 +0900

    [FEAT]
    버튼 랜덤 생성 구현

---
+++ Dungeon Rouge/Assets/Scripts/KKE/Map/MapGenerator.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class MapGenerator : MonoBehaviour
+{
+    public List<BtnData> prefabDataList;
+    public Transform pointSpot;
+
+    private void Start()
+    {
+        Transform[] generatorSpots = pointSpot.GetComponentsInChildren<Transform>();
+
+        foreach (Transform generatorSpot in generatorSpots)
+        {
+            if (generatorSpot != pointSpot)
+            {
+                GameObject selectedPrefab = GetRandomPrefab();
+                Instantiate(selectedPrefab, generatorSpot.position, generatorSpot.rotation, generatorSpot);
+            }
+        }
+    }
+
+    private GameObject GetRandomPrefab()
+    {
+        float totalProbability = 0f;
+
+        //모든 프리팹의 확률 합산하여 전체 확률 계산
+        foreach (BtnData data in prefabDataList)
+        {
+            totalProbability += data.probability;
+        }
+
+        //Random.value 값은 0 ~ 1 사이의 랜덤 값
+        //전체 확률과 Random.value 를 곱하여 랜덤 포인트 설정 ( 0 < randomPoint < totalProbability )
+        float randomPoint = Random.value * totalProbability;
+        float currentProbability = 0f;
+
+        //프리팹에 설정된 확률을 currentProbability에 합산해 randomPoint 보다 작을 경우 생성
+        foreach (BtnData data in prefabDataList)
+        {
+            currentProbability += data.probability;
+
+            if (randomPoint < currentProbability)
+            {
+                return data.prefab;
+            }
+        }
+
+        return null;
+    }
+}
