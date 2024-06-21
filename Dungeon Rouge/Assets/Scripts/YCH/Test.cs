c5ec17eaa05d36e9b94139896d8d07b7bc6797b0
CHICHI <smartb12@naver.com>
2024-06-21 20:03:21 +0900

    [FIX]
    skill

--- Dungeon Rouge/Assets/Scripts/YCH/Test.cs
+++ Dungeon Rouge/Assets/Scripts/YCH/Test.cs
@@ -4,18 +4,12 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    
     [SerializeField] StatData playerData;
     public CharacterStatHandler characterStatHandler;
 
     void Start()
     {
         characterStatHandler = GetComponent<CharacterStatHandler>();
-    }
-
-    void Update()
-    {
-
     }
 
     public void OnAttack()
