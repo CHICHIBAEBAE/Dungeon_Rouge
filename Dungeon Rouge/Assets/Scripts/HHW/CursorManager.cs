8a8bc06658f96092b1b426710e9f697ca580366d
hahw0627 <hahw0627@naver.com>
2024-06-24 22:42:04 +0900

    [Add]
    마우스 커서 추가

---
+++ Dungeon Rouge/Assets/Scripts/HHW/CursorManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    public Texture2D cursorIcon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.SetCursor(cursorIcon, Vector2.zero, CursorMode.Auto);
+    }
+}
