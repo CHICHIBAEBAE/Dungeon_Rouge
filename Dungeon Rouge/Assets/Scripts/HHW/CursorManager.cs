3e2e22be9ab2ef162b011ce75d7d1c3c8c287ca6
hahw0627 <hahw0627@naver.com>
2024-06-24 23:10:24 +0900

    [Add]
    커서 매니저 주석 추가

--- Dungeon Rouge/Assets/Scripts/HHW/CursorManager.cs
+++ Dungeon Rouge/Assets/Scripts/HHW/CursorManager.cs
@@ -8,6 +8,6 @@ public class CursorManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.SetCursor(cursorIcon, Vector2.zero, CursorMode.Auto);
+        Cursor.SetCursor(cursorIcon, Vector2.zero, CursorMode.Auto); // Cursor.SetCoursor 메서드를 통해 변경된 커서는 게임 전역에서 유지가 됨 -> 한 씬에서 커서를 설정하면 다른 씬으로 넘어가도 그 설정이 유지됨
     }
 }
